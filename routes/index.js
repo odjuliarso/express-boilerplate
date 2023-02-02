fc51d5548fd60931aea4f14d6e539a14f101cd17
Othman Djuliarso <othmandjuliarso@gmail.com>
2023-02-02 01:46:30 +0000

    fixed port in bin

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
-	res.send ('Goodday from express\n');
+router.get('/', (req, res,next) => {
+  res.send('Bye from express\n');
+	next();
 });
 
 module.exports = router;
